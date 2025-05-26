cedb6b718b6a07c6f919cbd02b4a7c99bf21f07f
saurabh <saurabhjain04r@gmail.com>
2025-05-27 03:48:14 +0530

    add login api with JWT authentication

--- src/main/java/com/taskcli/task_manager/controller/UserController.java
+++ src/main/java/com/taskcli/task_manager/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.taskcli.task_manager.controller;
 
+import com.taskcli.task_manager.dto.LoginRequest;
 import com.taskcli.task_manager.dto.RegisterRequest;
 import com.taskcli.task_manager.model.User;
+import com.taskcli.task_manager.service.JwtService;
 import com.taskcli.task_manager.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -12,9 +17,11 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final JwtService jwtService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtService jwtService) {
         this.userService = userService;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/register")
@@ -22,5 +29,20 @@ public class UserController {
         System.out.println("✅ Register API HIT");
         User savedUser = userService.registerUser(request);
         return ResponseEntity.ok(savedUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request){
+        try{
+            User user = userService.authenticateUser(request);
+            String token = jwtService.generateToken(user.getEmail());
+
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e){
+            return ResponseEntity.status(401).body(Map.of("error", e.getMessage()));
+        }
     }
 }
--- src/main/java/com/taskcli/task_manager/service/UserService.java
+++ src/main/java/com/taskcli/task_manager/service/UserService.java
@@ -1,9 +1,11 @@
 package com.taskcli.task_manager.service;
 
+import com.taskcli.task_manager.dto.LoginRequest;
 import com.taskcli.task_manager.dto.RegisterRequest;
 import com.taskcli.task_manager.Enum.Role;
 import com.taskcli.task_manager.model.User;
 import com.taskcli.task_manager.repository.UserRepository;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -24,5 +26,21 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setRole(Role.valueOf(request.getRole().toUpperCase()));
         return userRepository.save(user);
+    }
+
+    public User authenticateUser(LoginRequest request) {
+        Optional<User> optionalUser = userRepository.findByEmail(request.getEmail());
+
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+
+        User user = optionalUser.get();
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return user;
     }
 }
